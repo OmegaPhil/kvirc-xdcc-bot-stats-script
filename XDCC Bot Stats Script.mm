7180858551b3d64177563074570ac502a7753708
Omega Weapon <OmegaPhil@gmail.com>
2013-05-19 15:33:58 +0100

    Initial commit

---
+++ XDCC Bot Stats Script.mm
@@ -0,0 +1,50 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="XDCC Bot Stats Script" ID="ID_626258206" CREATED="1368970935263" MODIFIED="1368971081235" COLOR="#000000">
+<hook NAME="MapStyle" max_node_width="600"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<font SIZE="20"/>
+<node TEXT="ctcp &lt;bot&gt; status" POSITION="right" ID="ID_720389776" CREATED="1368966489859" MODIFIED="1368971097376" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="2258 606277 33425 9657029 6 200 0 500 0 0 107078.8 0 185396 1545.0 62586.3" ID="ID_203006197" CREATED="1368966509570" MODIFIED="1368971097401" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="See &apos;status&apos; command notes in official iroffer documentation" ID="ID_1774655036" CREATED="1368966894596" MODIFIED="1368971097416" COLOR="#00b439" LINK="http://iroffer.org/docs/admin.html">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="DCC chat status command" POSITION="right" ID="ID_1887367837" CREATED="1368968051252" MODIFIED="1368971097431" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="Stat: 11/100 Sls, 7/500 Q, 0/1000 I, 0/0 SrQ (Bdw: 835268K, 6960.6K/s, 0.0K/s Up, 6960.6K/s Down)" ID="ID_440305606" CREATED="1368968056530" MODIFIED="1368971097478" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="iroffer dinoex mod notes" POSITION="right" ID="ID_1230065998" CREATED="1368969529876" MODIFIED="1368971097497" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="DCC chat status" ID="ID_969698381" CREATED="1368969888183" MODIFIED="1368971097502" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="iroffer_misc.c:getstatusline" ID="ID_113092758" CREATED="1368969895543" MODIFIED="1368971097504" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="CTCP status" ID="ID_1327844152" CREATED="1368970182866" MODIFIED="1368971097507" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="iroffer_misc.c:getstatuslinenums" ID="ID_1365669095" CREATED="1368969895543" MODIFIED="1368971097509" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Have asked dinoex if its possible to extend the status line to include the idle queue (19.05.13 in #dinoex)" ID="ID_154820761" CREATED="1368971359258" MODIFIED="1368971384484" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
